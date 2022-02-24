aa510676b6d648f832fd4df9446f7df3f7f83487
Vishnu-Natraj <vishnuunatraj@gmail.com>
2022-02-24 10:10:02 +0530

    Initial commit

---
+++ src/logout/logout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lopgout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./lopgout.component.css']
+})
+export class LopgoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
